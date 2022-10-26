f8ef21b47fe365c1db014c4fc49c03d0ce7d2d26
isaacGrodriguez <falcohalcon@gmail.com>
2022-10-26 18:24:12 -0500

    Actualizacion 1.3 Final

--- main.js
+++ main.js
@@ -63,49 +63,39 @@ btnSubmitChakra.onclick = function(){calculoSubmitChakra()}
 function calculoSubmitChakra(){
     let chakraPJ = document.getElementById("inputReceptorTecnica").value
     let chakraTecnicaEnemigo = document.getElementById("inputTecnica").value
-    let calculo_tecnica = (chakraTecnicaEnemigo * 0.5)
-    let totalDañotecnica = (parseInt(chakraTecnicaEnemigo) + parseInt(calculo_tecnica))
-    let totalDañoChakra = (chakraPJ - totalDañotecnica)
-    if (totalDañoChakra < 0) totalDañoChakra = 0;
-    document.getElementById('resultadoTecnica').innerHTML = "El total de daño de la tecnica del rival es de " + totalDañotecnica + " U/C. \nTu chakra restante es de "+ totalDañoChakra + "U/C";
+    if (document.getElementById("radioChakraNormal").checked){
+        if(document.getElementById("radioBloqueo").checked){
+            let calculo_tecnica = (chakraTecnicaEnemigo * 0.4)
+            let totalDañoChakra = (chakraPJ - calculo_tecnica)
+            if (totalDañoChakra < 0) totalDañoChakra = 0
+            document.getElementById('resultadoTecnica').innerHTML = "El total de daño de la tecnica del rival es de " + calculo_tecnica + " U/C. \nTu chakra restante es de "+ totalDañoChakra + "U/C";
+            return;
+        }else{
+            let calculo_tecnica = (chakraTecnicaEnemigo * 0.5)
+            let totalDañotecnica = (parseInt(chakraTecnicaEnemigo) + parseInt(calculo_tecnica))
+            let totalDañoChakra = (chakraPJ - totalDañotecnica)
+            if (totalDañoChakra < 0) totalDañoChakra = 0
+            document.getElementById('resultadoTecnica').innerHTML = "El total de daño de la tecnica del rival es de " + totalDañotecnica + " U/C. \nTu chakra restante es de "+ totalDañoChakra + "U/C";
+            return;
+        } 
+    }else if (document.getElementById("radioSenjutsu").checked){
+        if(document.getElementById("radioBloqueo").checked){
+            let calculo_tecnica = (chakraTecnicaEnemigo * 0.5)
+            let totalDañotecnica = (parseInt(chakraTecnicaEnemigo) + parseInt(calculo_tecnica))
+            let calculo_senjutsu = (totalDañotecnica * 2)
+            let calculo_total_senjutsu = (calculo_senjutsu * 0.4)
+            let totalDañoChakra = (chakraPJ - calculo_total_senjutsu)
+            if (totalDañoChakra < 0) totalDañoChakra = 0
+            document.getElementById('resultadoTecnica').innerHTML = "El total de daño de la tecnica del rival es de " + calculo_total_senjutsu + " U/S. \nTu chakra restante es de "+ totalDañoChakra + "U/C";
+            return;
+        }else{
+            let calculo_tecnica = (chakraTecnicaEnemigo * 0.5)
+            let totalDañotecnica = (parseInt(chakraTecnicaEnemigo) + parseInt(calculo_tecnica))
+            let calculo_senjutsu = (totalDañotecnica * 2)
+            let totalDañoChakra = (chakraPJ - calculo_senjutsu)
+            if (totalDañoChakra < 0) totalDañoChakra = 0
+            document.getElementById('resultadoTecnica').innerHTML = "El total de daño de la tecnica del rival es de " + calculo_senjutsu + " U/S. \nTu chakra restante es de "+ totalDañoChakra + "U/C";
+            return;
+        } 
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let respuesta = prompt ("Bienvenido a la calculadora de daño \nIngrese que desea calcular (Golpes) (Tecnicas)") 
-
-// while (respuesta.toLowerCase() != "Golpes" || respuesta.toLowerCase() != "Tecnicas" ){
-//     if (respuesta == "golpes"){
-//         alert ("Los golpes y/o patadas dados con fuerza son multiplicados x2. El calculo se realiza restando la fuerza del golpe a la fuerza tuya, posteriormente añadiendo la multiplicacion para definir cuanto daño te realizo.")
-//         let fuerzaPJ = prompt ("Ingrese la fuerza de tu personaje")
-//         let fuerzaEnemigo = prompt ("Ingrese la fuerza del golpe recibido")
-//         let totalDañoFuerza = ((fuerzaEnemigo - fuerzaPJ) * 2)
-//         alert ("El daño total a tu personaje tras recibir "+fuerzaEnemigo+" KG de fuerza es de " +totalDañoFuerza +" KG de daño total.")
-//         break;
-//     } else if (respuesta == "tecnicas"){
-//         alert ("")
-//         let chakraPJ = prompt ("Ingrese la cantidad de chakra restante de tu personaje")
-//         let chakraEnemigo = prompt ("Ingrese la cantidad de chakra de la tecnica que te golpea.")
-//         let calculo_tecnica = (chakraEnemigo * 0.5)
-//         let totalDañotecnica = (parseInt(chakraEnemigo) + parseInt(calculo_tecnica))
-//         let totalDañoChakra = (chakraPJ - totalDañotecnica)
-//         alert ("El total de daño de la tecnica del rival es de " + totalDañotecnica + " U/C. \nTu chakra restante es de "+ totalDañoChakra + "U/C")
-//         break;
-//     } else {
-//         respuesta = prompt ("No podemos calcular eso, solo golpes o tecnicas \n ¿Que desea calcular?")
-//     }
-// }
